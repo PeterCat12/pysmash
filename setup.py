c2a6b3ade6322e67c9efdb06bd00dd5c8eebb115
Peter Wensel <pwensel@gmail.com>
2016-11-19 13:06:29 -0600

    fix readme

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ import pysmash
 setup(name="pysmash",
       author="Peter Wensel",
       url="https://github.com/PeterCat12/pysmash",
+      description="python bindings for Smash.gg API",
       version=pysmash.__version__,
       packages=[
           'pysmash',
