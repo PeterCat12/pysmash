a0b86e8c77569d15deebb231847ccbbb78902a93
Peter Wensel <pwensel@gmail.com>
2016-11-25 20:19:50 -0600

    delete comments

--- setup.py
+++ setup.py
@@ -13,19 +13,3 @@ setup(name="pysmash",
         'requests==2.12.1',
     ],
 )
-
-
-# from setuptools import setup
-#
-# setup(name='funniest',
-#       version='0.1',
-#       description='The funniest joke in the world',
-#       url='http://github.com/storborg/funniest',
-#       author='Flying Circus',
-#       author_email='flyingcircus@example.com',
-#       license='MIT',
-#       packages=['funniest'],
-#       install_requires=[
-#           'markdown',
-#       ],
-#       zip_safe=False)
